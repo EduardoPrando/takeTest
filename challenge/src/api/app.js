f86999c5f96a63e96de4f69f60dcd8c66c5c44b7
Eduardo Luiz Prando <eduardoprando159@hotmail.com>
2022-03-11 18:15:12 -0300

    feat: heroku

--- challenge/src/api/app.js
+++ challenge/src/api/app.js
@@ -4,12 +4,11 @@ const errorHandler = require('../middleware/errorHandler');
 const { responseFromGithub } = require('../middleware');
 
 const app = express();
-require('dotenv').config();
 
-const port = process.env.PORT || 3001;
+// const port = process.env.PORT || 3001;
 
 app.get('/', responseFromGithub);
 
 app.use(errorHandler);
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+module.exports = app;
